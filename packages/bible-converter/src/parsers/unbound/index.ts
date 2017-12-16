28ac6bdc18a31d68592cda6701967a601fee8317
Pavel Struhar <pstruhar@gmail.com>
2017-12-17 00:21:06 +0100

    Unbound: Parse column indexes from line starting with #columns

--- packages/bible-converter/src/parsers/unbound/index.ts
+++ packages/bible-converter/src/parsers/unbound/index.ts
@@ -16,33 +16,70 @@ const parse: ParserFunc = (data: string, name: string) => {
     stats: {}
   };
 
+  // Indexes of tab-separated columns initialized
+  let bookIndex = -1;
+  let chapterIndex = -1;
+  let verseIndex = -1;
+  let textIndex = -1;
+
   lines.forEach(line => {
     const normalLine = line.trim();
-    if (normalLine.length > 0 && normalLine[0] !== "#") {
+
+    if (normalLine.length > 0 && normalLine.indexOf("#columns") === 0) {
       const lineParts = normalLine.split("\t");
-      const bookNumber = parseInt(lineParts[0], 10);
-      const chapterNumber = parseInt(lineParts[1], 10);
-      const verseNumber = parseInt(lineParts[2], 10);
-      const verseText = lineParts[3];
+      lineParts.forEach((part, index) => {
+        switch (part) {
+          case "orig_book_index":
+            bookIndex = index - 1;
+            return;
+          case "orig_chapter":
+            chapterIndex = index - 1;
+            return;
+          case "orig_verse":
+            verseIndex = index - 1;
+            return;
+          case "text":
+            textIndex = index - 1;
+          default:
+            return;
+        }
+      });
+    }
 
-      // create book object if it does not exist (in stats, too)
-      if (bibleObj.books[booksOrder[bookNumber - 1]] === undefined) {
-        bibleObj.books[booksOrder[bookNumber - 1]] = {
-          chapters: []
-        };
-        bibleObj.stats[booksOrder[bookNumber - 1]] = [];
+    if (normalLine.length > 0 && normalLine[0] !== "#") {
+      if (bookIndex === -1) {
+        throw new Error(
+          "No line starting with #columns was encountered in this file."
+        );
       }
-      const book = bibleObj.books[booksOrder[bookNumber - 1]];
 
-      // create chapter object if it does not exist
-      if (book.chapters[chapterNumber - 1] === undefined) {
-        book.chapters[chapterNumber - 1] = {
-          verses: []
-        };
+      const lineParts = normalLine.split("\t");
+      const bookNumber = parseInt(lineParts[bookIndex], 10);
+      const chapterNumber = parseInt(lineParts[chapterIndex], 10);
+      const verseNumber = parseInt(lineParts[verseIndex], 10);
+      const verseText = lineParts[textIndex];
+
+      // For now, there is no support for apocryphas
+      if (bookNumber <= 66) {
+        // create book object if it does not exist (in stats, too)
+        if (bibleObj.books[booksOrder[bookNumber - 1]] === undefined) {
+          bibleObj.books[booksOrder[bookNumber - 1]] = {
+            chapters: []
+          };
+          bibleObj.stats[booksOrder[bookNumber - 1]] = [];
+        }
+        const book = bibleObj.books[booksOrder[bookNumber - 1]];
+
+        // create chapter object if it does not exist
+        if (book.chapters[chapterNumber - 1] === undefined) {
+          book.chapters[chapterNumber - 1] = {
+            verses: []
+          };
+        }
+        const chapter = book.chapters[chapterNumber - 1];
+
+        chapter.verses[verseNumber - 1] = verseText;
       }
-      const chapter = book.chapters[chapterNumber - 1];
-
-      chapter.verses[verseNumber - 1] = verseText;
     }
   });
 
