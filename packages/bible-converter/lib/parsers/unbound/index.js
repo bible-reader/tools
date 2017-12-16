867d67d1c5e2fd5b6d4689ddabb7b26ad574bc7d
Pavel Struhar <pstruhar@gmail.com>
2017-12-17 00:24:03 +0100

    Dist files for the last change

--- packages/bible-converter/lib/parsers/unbound/index.js
+++ packages/bible-converter/lib/parsers/unbound/index.js
@@ -12,30 +12,61 @@ var parse = function (data, name) {
         books: {},
         stats: {}
     };
+    // Indexes of tab-separated columns initialized
+    var bookIndex = -1;
+    var chapterIndex = -1;
+    var verseIndex = -1;
+    var textIndex = -1;
     lines.forEach(function (line) {
         var normalLine = line.trim();
-        if (normalLine.length > 0 && normalLine[0] !== "#") {
+        if (normalLine.length > 0 && normalLine.indexOf("#columns") === 0) {
             var lineParts = normalLine.split("\t");
-            var bookNumber = parseInt(lineParts[0], 10);
-            var chapterNumber = parseInt(lineParts[1], 10);
-            var verseNumber = parseInt(lineParts[2], 10);
-            var verseText = lineParts[3];
-            // create book object if it does not exist (in stats, too)
-            if (bibleObj.books[booksOrder[bookNumber - 1]] === undefined) {
-                bibleObj.books[booksOrder[bookNumber - 1]] = {
-                    chapters: []
-                };
-                bibleObj.stats[booksOrder[bookNumber - 1]] = [];
+            lineParts.forEach(function (part, index) {
+                switch (part) {
+                    case "orig_book_index":
+                        bookIndex = index - 1;
+                        return;
+                    case "orig_chapter":
+                        chapterIndex = index - 1;
+                        return;
+                    case "orig_verse":
+                        verseIndex = index - 1;
+                        return;
+                    case "text":
+                        textIndex = index - 1;
+                    default:
+                        return;
+                }
+            });
+        }
+        if (normalLine.length > 0 && normalLine[0] !== "#") {
+            if (bookIndex === -1) {
+                throw new Error("No line starting with #columns was encountered in this file.");
             }
-            var book = bibleObj.books[booksOrder[bookNumber - 1]];
-            // create chapter object if it does not exist
-            if (book.chapters[chapterNumber - 1] === undefined) {
-                book.chapters[chapterNumber - 1] = {
-                    verses: []
-                };
+            var lineParts = normalLine.split("\t");
+            var bookNumber = parseInt(lineParts[bookIndex], 10);
+            var chapterNumber = parseInt(lineParts[chapterIndex], 10);
+            var verseNumber = parseInt(lineParts[verseIndex], 10);
+            var verseText = lineParts[textIndex];
+            // For now, there is no support for apocryphas
+            if (bookNumber <= 66) {
+                // create book object if it does not exist (in stats, too)
+                if (bibleObj.books[booksOrder[bookNumber - 1]] === undefined) {
+                    bibleObj.books[booksOrder[bookNumber - 1]] = {
+                        chapters: []
+                    };
+                    bibleObj.stats[booksOrder[bookNumber - 1]] = [];
+                }
+                var book = bibleObj.books[booksOrder[bookNumber - 1]];
+                // create chapter object if it does not exist
+                if (book.chapters[chapterNumber - 1] === undefined) {
+                    book.chapters[chapterNumber - 1] = {
+                        verses: []
+                    };
+                }
+                var chapter = book.chapters[chapterNumber - 1];
+                chapter.verses[verseNumber - 1] = verseText;
             }
-            var chapter = book.chapters[chapterNumber - 1];
-            chapter.verses[verseNumber - 1] = verseText;
         }
     });
     // Stats: number of verses for each chapter
