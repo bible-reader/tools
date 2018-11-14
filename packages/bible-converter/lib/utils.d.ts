16c973468e762f7729b09782a9d2b1f5ab22055b
Pavel Struhar <pstruhar@gmail.com>
2018-11-15 00:14:25 +0100

    Update typescript, jest, ts-jest

--- packages/bible-converter/lib/utils.d.ts
+++ packages/bible-converter/lib/utils.d.ts
@@ -1,5 +1,5 @@
 export declare function pad(n: string, width: number, z?: string): string;
 declare const _default: {
-    pad: (n: string, width: number, z?: string | undefined) => string;
+    pad: typeof pad;
 };
 export default _default;
--- packages/reading-plan-validator/lib/index.d.ts
+++ packages/reading-plan-validator/lib/index.d.ts
@@ -1,7 +1,8 @@
 import { Versification, Passage } from "@bible-reader/types";
-declare function validateReadingPlan(passages: Array<Passage>, versification: Versification, checkContinuity?: boolean): {
+declare type Error = {
     passageIndex: number;
     isError: boolean;
     message: string;
-}[];
+};
+declare function validateReadingPlan(passages: Array<Passage>, versification: Versification, checkContinuity?: boolean): Error[];
 export default validateReadingPlan;
