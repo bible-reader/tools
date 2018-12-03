7a98f387a8ff3346e8603fdec60d874237f0f57d
Pavel Struhar <pstruhar@gmail.com>
2018-12-03 23:50:44 +0100

    Remove reference-validator, functionality was moved to v11n-tools

--- packages/reference-validator/lib/index.d.ts
+++
@@ -1,3 +0,0 @@
-import { Versification } from "@bible-reader/types";
-export declare function chapterExists(v11n: Versification, book: string, chapter: number): boolean;
-export declare function verseExists(v11n: Versification, book: string, chapter: number, verse: number): boolean;
